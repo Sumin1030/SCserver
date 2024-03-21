080e63ced9657f67f38a7323984a5dd2de72b940
sumin <gjtnals258@gmail.com>
2024-03-21 00:19:33 -0400

    test

--- db.js
+++ db.js
@@ -3,7 +3,7 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
+    host: '18.217.16.111',
     user: 'admin',
     password: 'canadasumin',
     port: 3306,
