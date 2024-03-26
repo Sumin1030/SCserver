041c2ff3839d84bdfa2a558bc43c947712fd4073
sumin <gjtnals258@gmail.com>
2024-03-26 03:07:02 -0400

    test

--- server.js
+++ server.js
@@ -8,18 +8,18 @@ const cookieParser = require('cookie-parser')
 const port=5001; //React가 3000번 포트를 사용하기 때문에 node 서버가 사용할 포트넘버는 다른 넘버로 지정해준다.
 app.listen(port, ()=>{console.log(`Listening on port ${port}`);});
 
+// body로 전달되는 내용 파싱. express 내장 모듈
+// json 형태 데이터 전달(raw, text 등도 있음.)
+app.use(express.json());
+// url 형태 데이터 전달, extended: 확장된 queryString 모듈 사용 여부
+// app.use(express.urlencoded({extended: false}));
+
 app.use(cors({
     credentials: true,
     // origin: 'http://localhost:3000'
     origin: 'https://thisissumin.store',
     methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD"]
 }));
-
-// body로 전달되는 내용 파싱. express 내장 모듈
-// json 형태 데이터 전달(raw, text 등도 있음.)
-app.use(express.json());
-// url 형태 데이터 전달, extended: 확장된 queryString 모듈 사용 여부
-// app.use(express.urlencoded({extended: false}));
 
 app.use('/image', express.static('./upload'));
 
